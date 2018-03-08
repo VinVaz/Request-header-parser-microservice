0bf178243d78fcced202c8dc30596bcdce348b5d
VinVaz <viniciusvazpro@gmail.com>
2018-03-08 13:45:48 -0300

    updating the program

--- TimestampMicroservice.js
+++ TimestampMicroservice.js
@@ -3,12 +3,28 @@ var moment = require('moment');
 
 http.createServer(function(req, res){
 	
-	var givenTime = decodeURI(req.url);
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	var myMoment = moment(givenTime, "MMMM D, YYYY");
-	console.log(myMoment);
-	//res.write(myMoment);
-    res.end();
+	if(req.url != '/favicon.ico'){
 	
-	}).listen(8080);
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  var givenTime = decodeURI(req.url);
+	  
+	  var myMomentOne = moment(givenTime, "MMMM D, YYYY");
+	  var myMomentTwo = moment.unix(givenTime);
+      var myMoment = {};
+	  
+	  if(myMomentOne.isValid()) myMoment = myMomentOne.format(); 
+	  else if(myMomentTwo.isValid()) myMoment = myMomentTwo; 
+	  else console.log("Err"); 
+	  
+	  console.log(myMoment);
+	  /*
+	  var output = {
+		  unix: myMoment.unix,
+		  natural: myMoment.natural
+	  }*/
+	  //res.write(myMoment);
+      res.end();
+	}
+	
+}).listen(8080);
 	
