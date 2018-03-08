28bcf900ef4071d6a81e4b8dfcef387db106ab78
VinVaz <viniciusvazpro@gmail.com>
2018-03-08 14:48:01 -0300

    uses regex to test the type of date the program receives

--- TimestampMicroservice.js
+++ TimestampMicroservice.js
@@ -8,18 +8,22 @@ http.createServer(function(req, res){
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
 	  //decode the given url that represents a date 
 	  var givenTime = decodeURI(req.url);
-	  
-	  //changes the received date and covert to a moment format
-	  var myMomentOne = moment(givenTime, "MMMM D, YYYY");
-	  var myMomentTwo = moment.unix('1450137600');
-      var myMoment = {};
+	  var firstFilter = /time/;
+	  var secondFilter = /time/;
+	  var firstTest = firstFilter.test(givenTime);
+	  var secondTest = secondFilter.test(givenTime);
+	  var myMoment = {};
 	  
 	  //check if the date is either a unix timestamp or 
-	  //a natural language form of date
-	  if(myMomentOne.isValid()) myMoment = myMomentOne.format(); 
-	  else if(myMomentTwo.isValid()) myMoment = myMomentTwo.format(); 
-	  else console.log("Err"); 
+	  //a natural language form of date and then
+	  //changes the received date and covert to a moment format
+	  if(firstTest){
+        myMoment = moment(givenTime, "MMMM D, YYYY").format();;  
+	  }else if(secondTest){
+	    myMoment = moment.unix('1450137600');
+	  }else console.log("Err"); 
 	  
+	
 	  var output = {
 		  unix: moment(myMoment).unix(),
 		  natural: moment(myMoment).format("MMMM D, YYYY")
