8f6cbf5e64015b449bc42127c246991ec87120af
VinVaz <viniciusvazpro@gmail.com>
2018-03-08 14:18:55 -0300

    updating comments and the output section of the program

--- TimestampMicroservice.js
+++ TimestampMicroservice.js
@@ -6,22 +6,26 @@ http.createServer(function(req, res){
 	if(req.url != '/favicon.ico'){
 	
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  //decode the given url that represents a date 
 	  var givenTime = decodeURI(req.url);
 	  
+	  //changes the received date and covert to a moment format
 	  var myMomentOne = moment(givenTime, "MMMM D, YYYY");
-	  var myMomentTwo = moment.unix(givenTime);
+	  var myMomentTwo = moment.unix('1450137600');
       var myMoment = {};
 	  
+	  //check if the date is either a unix timestamp or 
+	  //a natural language form of date
 	  if(myMomentOne.isValid()) myMoment = myMomentOne.format(); 
-	  else if(myMomentTwo.isValid()) myMoment = myMomentTwo; 
+	  else if(myMomentTwo.isValid()) myMoment = myMomentTwo.format(); 
 	  else console.log("Err"); 
 	  
-	  console.log(myMoment);
-	  /*
 	  var output = {
-		  unix: myMoment.unix,
-		  natural: myMoment.natural
-	  }*/
+		  unix: moment(myMoment).unix(),
+		  natural: moment(myMoment).format("MMMM D, YYYY")
+	  }
+	  	  console.log(output);
+	  
 	  //res.write(myMoment);
       res.end();
 	}
