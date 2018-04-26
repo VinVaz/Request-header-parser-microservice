8be46d5a0014be36559f85e29451e00b11a52163
VinVaz <viniciusvazpro@gmail.com>
2018-04-26 20:24:20 -0300

    update

--- requestHeaderParser.js
+++ requestHeaderParser.js
@@ -1,33 +1,31 @@
 var http = require('http');
 var os = require("os");
-//var ip = require("ip");
-
 
 var port = process.env.PORT || 8080;
 var IP_adress = 0;
 var language = "";
 var operatingSystem = "";
 
-  http.createServer(function(req, res){
-	   
-	IP_adress = req.headers['x-forwarded-for'] ||
+  http.createServer(function(req, res){	
+
+    IP_adress = req.headers['x-forwarded-for'] ||
             	req.connection.remoteAddress || 
 				req.socket.remoteAddress || 
 				req.connection.socket.remoteAddress;
     
-	language = req.headers["accept-language"];
-	language = language.split(";")[0];
-	language = language.split(",")[0];
+    language = req.headers["accept-language"];
+    language = language.split(";")[0];
+    language = language.split(",")[0];
 	
-	var agent = req.headers["user-agent"];
-	myRegex = /(?:[(])([^(]*)(?:[)])/;
+    var agent = req.headers["user-agent"];
+    myRegex = /(?:[(])([^(]*)(?:[)])/;
     operatingSystem = myRegex.exec(agent)[1];
 	   
-	var output = {
-		ipaddress: IP_adress,
-		language: language,
-		software: operatingSystem
-	}   
+    var output = {
+      ipaddress: IP_adress,
+      language: language,
+      software: operatingSystem
+    }   
 	   
 	  res.write(JSON.stringify(output));
       res.end();
